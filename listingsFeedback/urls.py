51a260e24b91b7d63c58792fabcadccaec8dc05a
AbdullahRehman123 <abdullahrahmandp@gmail.com>
2022-03-31 22:18:16 +0500

    Commit Message

--- listingsFeedback/urls.py
+++ listingsFeedback/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('listingfeedbacks-all', views.SpecificListingFeedback.as_view()),
     path('listingfeedbacks-all/<int:pk>', views.ListingFeedbackDetail.as_view()),
-    #path('listings-all/listingfeedbacks-all/', views.SpecificListingFeedbacks.as_view()),
-    #http://example.com/api/purchases
+    path('listings-all/listingfeedbacks-all/', views.SpecificListingFeedback.as_view()),
 ]
