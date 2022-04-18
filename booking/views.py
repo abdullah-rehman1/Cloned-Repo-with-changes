1f51d02184cbbce5693e282c5e2f3e034eea8edd
AbdullahRehman123 <abdullahrahmandp@gmail.com>
2022-04-18 06:06:17 +0500

    Added tenant_id in SpecificListingBooking at Booking:view.py

--- booking/views.py
+++ booking/views.py
@@ -17,6 +17,10 @@ class SpecificListingBooking(generics.ListCreateAPIView):
     def get_queryset(self):
         queryset = Booking.objects.all()
         list_id = self.request.query_params.get('list_id')
+        tenant_id = self.request.query_params.get('tenant_id')
         if list_id is not None:
             queryset = queryset.filter(list_id_id=list_id)
+        elif tenant_id is not None:
+                queryset = queryset.filter(tenant_id_id=tenant_id)
         return queryset
+        
